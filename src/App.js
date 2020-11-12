734c9b9eb1ed3624ed6544e320858b676af4abeb
Iyad <iyadhb@gmail.com>
2020-11-12 20:16:07 +0000

    useEffect start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Setup from './tutorial/2-useEffect/setup/1-useEffect-basics'
 
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced Tutorial</h2>
+      <Setup />
     </div>
   )
 }

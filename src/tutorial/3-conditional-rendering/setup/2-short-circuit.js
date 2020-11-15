173ecd5befb66cce5af20d632377540bbfadbcc8
Iyad MPRO <iyadhb@gmail.com>
2020-11-15 23:29:34 +0000

    Ternary Operator

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -4,15 +4,19 @@ import React, { useState } from 'react';
 
 const ShortCircuit = () => {
   const [text, setText] = useState('')
-  const firstValue = text || 'hello world';
-  const secondValue = text && 'hello world';
+  const [isError, setIsError] = useState(false)
+  // const firstValue = text || 'hello world';
+  // const secondValue = text && 'hello world';
 
   return <>
     {/* <h1>{firstValue}</h1>
     <h1>value: {secondValue}</h1> */}
     <h1>{text || 'Iyad Barakat'}</h1>
-    {text && <h1>Hello World</h1>}
-    {!text && <h1>There is no text</h1>}
+    <button className="btn" onClick={() => setIsError(!isError)}>toggle error</button>
+    {isError && <h1>Error...</h1>}
+    {isError ? <p>there is an error</p> : <div>
+      <h2>there is no errors</h2></div>}
+
   </>;
 };
 
